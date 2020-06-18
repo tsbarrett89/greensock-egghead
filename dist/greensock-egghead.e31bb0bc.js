04a64ae4254f049dd9b30c75060aad2ab011726b
tsbarrett89 <tsbarrett89@gmail.com>
2020-06-17 22:48:20 -0600

    removed uneccessary animations

--- dist/greensock-egghead.e31bb0bc.js
+++ dist/greensock-egghead.e31bb0bc.js
@@ -5613,38 +5613,7 @@ _gsap.gsap.to(divs, 10, {
 
 document.addEventListener("click", function (event) {
   _gsap.gsap.killTweensOf(event.target);
-}); // TweenMax.set("#box", {
-//     backgroundColor: "green",
-//     width: "50px",
-//     height: "50px",
-//     x: "50px",
-//     y: "50px"
-// })
-// const timeline = new TimelineMax({ repeat: -1 })
-// timeline.pause()
-// timeline.to("#box", 0.5, { x: 100 })
-// timeline.to("#box", 0.5, { y: 100 })
-// timeline.to("#box", 0.5, { x: 50 })
-// timeline.to("#box", 0.5, { y: 50 })
-// document.getElementById("box").addEventListener('click', (e) => {
-//     e.stopPropagation()
-//     if(timeline.isActive()){
-//         timeline.pause()
-//     }else{
-//         timeline.resume()
-//     }
-// })
-// document.addEventListener('wheel', event => {
-//     if(event.wheelDelta > 0){
-//         TweenMax.to(timeline, .25, {progress: "+=0.1"})
-//     } else {
-//         TweenMax.to(timeline, .25, {progress: "-=0.1"})
-//     }
-// })
-// document.addEventListener('click', event => {
-//     const { x, y } = event
-//     TweenMax.fromTo('#box', 1, { x, y }, { x: 200, y: 200 })
-// })
+});
 },{"gsap":"node_modules/gsap/index.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
